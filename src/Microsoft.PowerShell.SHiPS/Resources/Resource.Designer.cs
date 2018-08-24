657b08bc9769e54878268f2392a9c250a5d34374
Jianyun Tao <jianyunt@microsoft.com>
2018-08-24 15:55:48 -0700

    fixed build

--- src/Microsoft.PowerShell.SHiPS/Resources/Resource.Designer.cs
+++ src/Microsoft.PowerShell.SHiPS/Resources/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.PowerShell.SHiPS {
+namespace Microsoft.PowerShell.SHiPS.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Microsoft.PowerShell.SHiPS {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.PowerShell.SHiPS.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.PowerShell.SHiPS.Resources", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
